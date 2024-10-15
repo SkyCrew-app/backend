8946c87278ed10d2437a351dba0db09b31b5a6b0
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-10-15 18:44:13 +0200

    feat: add new mailer function

--- src/modules/mail/mailer.service.ts
+++ src/modules/mail/mailer.service.ts
@@ -1,5 +1,8 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Transporter } from 'nodemailer';
+import * as Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class MailerService {
@@ -11,8 +14,18 @@ export class MailerService {
     to: string,
     subject: string,
     text: string,
-    html?: string,
+    templateName: string,
+    variables: { [key: string]: any },
   ): Promise<void> {
+    const templatePath = join(
+      __dirname,
+      '..',
+      '..',
+      'templates',
+      `${templateName}.hbs`,
+    );
+    const html = this.loadTemplate(templatePath, variables);
+
     await this.transporter.sendMail({
       from: process.env.MAIL_FROM,
       to,
@@ -20,5 +33,15 @@ export class MailerService {
       text,
       html,
     });
+  }
+
+  // Charger et compiler le template avec Handlebars
+  private loadTemplate(
+    templatePath: string,
+    variables: { [key: string]: any },
+  ): string {
+    const templateFile = readFileSync(templatePath, 'utf8');
+    const template = Handlebars.compile(templateFile);
+    return template(variables);
   }
 }
