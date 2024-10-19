e03485ee7d3ecb694433375bd202d62663504d1b
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-10-19 23:10:09 +0200

    feat: add new feature

--- src/modules/reservations/reservations.module.ts
+++ src/modules/reservations/reservations.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReservationsService } from './reservations.service';
 import { ReservationsResolver } from './reservations.resolver';
+import { Reservation } from './entity/reservations.entity';
+import { MailerModule } from '../mail/mailer.module';
+import { User } from '../users/entity/users.entity';
+import { Aircraft } from '../aircraft/entity/aircraft.entity';
 
 @Module({
-  providers: [ReservationsService, ReservationsResolver]
+  imports: [
+    TypeOrmModule.forFeature([Reservation, User, Aircraft]),
+    MailerModule,
+  ],
+  providers: [ReservationsService, ReservationsResolver],
+  exports: [ReservationsService],
 })
 export class ReservationsModule {}
