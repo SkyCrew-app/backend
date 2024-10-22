2cc0f385bfb50dab68f020a3adfdc2d54bef0640
MrBartou <53816747+MrBartou@users.noreply.github.com>
2024-10-22 14:49:59 +0200

    test: resolver test

--- src/modules/administration/administration.resolver.spec.ts
+++ src/modules/administration/administration.resolver.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdministrationResolver } from './administration.resolver';
+import { UsersService } from '../users/users.service';
+import { AdministrationService } from './administration.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AdministrationResolver', () => {
   let resolver: AdministrationResolver;
 
+  const mockJwtService = {
+    sign: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AdministrationResolver],
+      providers: [
+        AdministrationResolver,
+        UsersService,
+        AdministrationService,
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
     }).compile();
 
     resolver = module.get<AdministrationResolver>(AdministrationResolver);
--- src/modules/maintenance/maintenance.resolver.spec.ts
+++ src/modules/maintenance/maintenance.resolver.spec.ts
@@ -1,15 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MaintenanceResolver } from './maintenance.resolver';
+import { MaintenanceService } from './maintenance.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Maintenance } from './entity/maintenance.entity';
+import { AircraftService } from '../aircraft/aircraft.service';
 
 describe('MaintenanceResolver', () => {
   let resolver: MaintenanceResolver;
+  let service: MaintenanceService;
+
+  const mockMaintenanceRepository = {
+    find: jest.fn(),
+  };
+
+  const mockAircraftService = {
+    findOne: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MaintenanceResolver],
+      providers: [
+        MaintenanceResolver,
+        MaintenanceService,
+        {
+          provide: getRepositoryToken(Maintenance),
+          useValue: mockMaintenanceRepository,
+        },
+        {
+          provide: AircraftService,
+          useValue: mockAircraftService,
+        },
+      ],
     }).compile();
 
     resolver = module.get<MaintenanceResolver>(MaintenanceResolver);
+    service = module.get<MaintenanceService>(MaintenanceService);
   });
 
   it('should be defined', () => {
