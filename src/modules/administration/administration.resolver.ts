08b267c51fe3896f115f84e3d26ec5f52a3c563e
MrBartou <anthony.denin@outlook.com>
2024-11-22 14:26:29 +0100

    feat: :sparkles: Add administration feature

--- src/modules/administration/administration.resolver.ts
+++ src/modules/administration/administration.resolver.ts
@@ -1,6 +1,39 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { AdministrationService } from './administration.service';
+import { Administration } from './entity/admin.entity';
+import { CreateAdministrationInput } from './dto/create-admin.input';
+import { UpdateAdministrationInput } from './dto/update-admin.input';
 
-@Resolver()
+@Resolver(() => Administration)
 export class AdministrationResolver {
-  constructor() {}
+  constructor(private readonly administrationService: AdministrationService) {}
+
+  @Query(() => [Administration], { name: 'getAllAdministrations' })
+  findAll() {
+    return this.administrationService.findAll();
+  }
+
+  @Mutation(() => Administration, { name: 'createAdministration' })
+  create(
+    @Args('createAdministrationInput')
+    createAdministrationInput: CreateAdministrationInput,
+  ): Promise<Administration> {
+    return this.administrationService.create(createAdministrationInput);
+  }
+
+  @Mutation(() => Administration, { name: 'updateAdministration' })
+  update(
+    @Args('updateAdministrationInput')
+    updateAdministrationInput: UpdateAdministrationInput,
+  ) {
+    return this.administrationService.update(
+      updateAdministrationInput.id,
+      updateAdministrationInput,
+    );
+  }
+
+  @Mutation(() => Boolean, { name: 'deleteAdministration' })
+  delete(@Args('id', { type: () => Int }) id: number) {
+    return this.administrationService.remove(id);
+  }
 }
