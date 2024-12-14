35794236c3ee62372677583728713bbf709a3d50
MrBartou <anthony.denin@outlook.com>
2024-12-14 15:16:13 +0100

    feat: fiw CORS

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://staging.skycrew.fr', 'http://localhost:3000'],
+    origin: ['https://staging.skycrew.fr', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
