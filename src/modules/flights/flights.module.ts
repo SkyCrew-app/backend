d2581eb7eb6ef813227ad356fff2178c447e7459
MrBartou <anthony.denin@outlook.com>
2025-01-03 13:41:45 +0100

    feat: :sparkles: New module for flight

--- src/modules/flights/flights.module.ts
+++ src/modules/flights/flights.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { FlightsService } from './flights.service';
 import { FlightsResolver } from './flights.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Flight } from './entity/flights.entity';
+import { Reservation } from '../reservations/entity/reservations.entity';
 
 @Module({
-  providers: [FlightsService, FlightsResolver]
+  imports: [TypeOrmModule.forFeature([Flight, Reservation])],
+  exports: [FlightsService],
+  providers: [FlightsService, FlightsResolver],
 })
 export class FlightsModule {}
