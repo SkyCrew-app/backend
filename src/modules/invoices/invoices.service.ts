67a2e0bfe98d176e6f5bffa28468733265e2f3d2
MrBartou <anthony.denin@outlook.com>
2025-01-06 20:30:29 +0100

    feat: :sparkles: add new invoice module

--- src/modules/invoices/invoices.service.ts
+++ src/modules/invoices/invoices.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Invoice } from './entity/invoices.entity';
+import { CreateInvoiceInput } from './dto/create-invoice.input';
+import { UpdateInvoiceInput } from './dto/update-invoice.input';
 
 @Injectable()
-export class InvoicesService {}
+export class InvoicesService {
+  constructor(
+    @InjectRepository(Invoice)
+    private invoicesRepository: Repository<Invoice>,
+  ) {}
+
+  async create(createInvoiceInput: CreateInvoiceInput): Promise<Invoice> {
+    const invoice = this.invoicesRepository.create({
+      ...createInvoiceInput,
+      payment_status: 'paid',
+    });
+    return this.invoicesRepository.save(invoice);
+  }
+
+  async findAll(): Promise<Invoice[]> {
+    return this.invoicesRepository.find({
+      relations: ['user', 'payments'],
+    });
+  }
+
+  async findOne(id: number): Promise<Invoice> {
+    return this.invoicesRepository.findOne({
+      where: { id },
+      relations: ['user', 'payments'],
+    });
+  }
+
+  async update(
+    id: number,
+    updateInvoiceInput: UpdateInvoiceInput,
+  ): Promise<Invoice> {
+    await this.invoicesRepository.update(id, updateInvoiceInput);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<boolean> {
+    const result = await this.invoicesRepository.delete(id);
+    return result.affected > 0;
+  }
+
+  async findByUser(userId: number): Promise<Invoice[]> {
+    return this.invoicesRepository.find({
+      where: { user: { id: userId } },
+      relations: ['payments'],
+    });
+  }
+}
