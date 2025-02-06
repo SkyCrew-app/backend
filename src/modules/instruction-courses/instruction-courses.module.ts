174781ab25aad00866e69534109276bbf7a12b84
MrBartou <anthony.denin@outlook.com>
2025-02-06 16:07:26 +0100

    feat: :sparkles: Intruction feature

--- src/modules/instruction-courses/instruction-courses.module.ts
+++ src/modules/instruction-courses/instruction-courses.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InstructionCourse } from './entity/instruction-courses.entity';
+import { CourseCompetency } from './entity/course-competency.entity';
+import { CourseComment } from './entity/course-comment.entity';
 import { InstructionCoursesService } from './instruction-courses.service';
 import { InstructionCoursesResolver } from './instruction-courses.resolver';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [InstructionCoursesService, InstructionCoursesResolver]
+  imports: [
+    TypeOrmModule.forFeature([
+      InstructionCourse,
+      CourseCompetency,
+      CourseComment,
+    ]),
+    UsersModule,
+  ],
+  providers: [InstructionCoursesService, InstructionCoursesResolver],
+  exports: [InstructionCoursesService],
 })
 export class InstructionCoursesModule {}
