21b781c00f4b4052b140c6e1d53bf5717a2c6d3e
MrBartou <anthony.denin@outlook.com>
2025-02-07 10:07:06 +0100

    fix: :ambulance: change for url database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,15 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ConfigService } from '@nestjs/config';
 
-export const typeOrmConfig = (
-  configService: ConfigService,
-): TypeOrmModuleOptions => ({
+export const typeOrmConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: parseInt(configService.get('DB_PORT')),
-  username: configService.get('DB_USER'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_NAME'),
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
