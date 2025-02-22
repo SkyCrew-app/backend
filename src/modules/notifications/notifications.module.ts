65746b326dd6064b1ae1c0729908b9a68052bc9b
MrBartou <anthony.denin@outlook.com>
2025-02-22 18:37:07 +0100

    feat: :sparkles: add notification module

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsResolver } from './notifications.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './entity/notifications.entity';
+import { NotificationsGateway } from './notifications.gateway';
 
 @Module({
-  providers: [NotificationsService, NotificationsResolver]
+  imports: [TypeOrmModule.forFeature([Notification])],
+  providers: [
+    NotificationsService,
+    NotificationsResolver,
+    NotificationsGateway,
+  ],
+  exports: [NotificationsService],
 })
 export class NotificationsModule {}
--- src/modules/notifications/notifications.resolver.ts
+++ src/modules/notifications/notifications.resolver.ts
@@ -1,4 +1,55 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { NotificationsService } from './notifications.service';
+import { Notification } from './entity/notifications.entity';
+import { CreateNotificationInput } from './dto/create-notification.input';
+import { UpdateNotificationInput } from './dto/update-notification.input';
 
-@Resolver()
-export class NotificationsResolver {}
+@Resolver(() => Notification)
+export class NotificationsResolver {
+  constructor(private readonly notificationsService: NotificationsService) {}
+
+  @Mutation(() => Notification, { name: 'createNotification' })
+  async createNotification(
+    @Args('createNotificationInput')
+    createNotificationInput: CreateNotificationInput,
+  ): Promise<Notification> {
+    return await this.notificationsService.create(createNotificationInput);
+  }
+
+  @Query(() => [Notification], { name: 'notificationsByUser' })
+  async notificationsByUser(
+    @Args('userId', { type: () => Int }) userId: number,
+  ): Promise<Notification[]> {
+    return await this.notificationsService.findAllByUser(userId);
+  }
+
+  @Query(() => Notification, { name: 'notification' })
+  async notification(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<Notification> {
+    return await this.notificationsService.findOne(id);
+  }
+
+  @Mutation(() => Notification, { name: 'updateNotification' })
+  async updateNotification(
+    @Args('updateNotificationInput')
+    updateNotificationInput: UpdateNotificationInput,
+  ): Promise<Notification> {
+    return await this.notificationsService.update(updateNotificationInput);
+  }
+
+  @Mutation(() => Boolean, { name: 'removeNotification' })
+  async removeNotification(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<boolean> {
+    return await this.notificationsService.remove(id);
+  }
+
+  //seen notification
+  @Mutation(() => Boolean, { name: 'seenNotification' })
+  async seenNotification(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<boolean> {
+    return await this.notificationsService.seenNotification(id);
+  }
+}
